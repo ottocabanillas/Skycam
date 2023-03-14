d48b07d5a2dfddf1d3610ca5fd64d214efa59c52
turco de mitri <guillermodemitriossuet@gmail.com>
2023-03-13 21:39:18 -0300

    [Added] X,Y and Z axis speeds check within field boundaries

--- Assets/SkycamController.cs
+++ Assets/SkycamController.cs
@@ -31,7 +31,10 @@ public class SkycamController : MonoBehaviour
                   currentSpeed;
 
     private bool isLeftTriggerPressed, // flag trigger izquierdo
-                 isRightTriggerPressed; // flag trigger derecho
+                 isRightTriggerPressed, // flag trigger derecho
+                 xBoundaryReached, // flag para limite del campo en el eje X (+/- X)
+                 yBoundaryReached, // flag para limite del campo en el eje Y (+/- Y)
+                 zBoundaryReached; // flag para limite del campo en el eje Z (+/- Z)
     void Start()
     {
         // Intenta traer el valor de velocidad máxima desde PlayerPrefs. Si no es nulo,
@@ -44,15 +47,18 @@ public class SkycamController : MonoBehaviour
 
         // Inicializamos ambos flags en false
         isLeftTriggerPressed = isRightTriggerPressed = false;
+
+        // Inicializamos los flags de limites del campo en false
+        xBoundaryReached = yBoundaryReached = zBoundaryReached = false;    
     }
 
     // Functions
     void Update()
     {
         _horizontalInput = Input.GetAxis("Horizontal"); //(Axis +/- X)
-        
+
         _verticalInput = Input.GetAxis("Vertical"); //(Axis +/- Z)
-        
+
         float yAxisMovement = 0;
 
         if (Gamepad.current != null) 
@@ -66,35 +72,39 @@ public class SkycamController : MonoBehaviour
             yAxisMovement = isLeftTriggerPressed ? -leftTriggerValue : isRightTriggerPressed ? rightTriggerValue : 0f;
         }
 
-        //Velocidad de cada eje
-        _currentSpeed_X = _speedMax * _horizontalInput;  // Velocidad en el eje X
-        _currentSpeed_Z = _speedMax * _verticalInput; // Velocidad en el eje Z
-        _currentSpeed_Y = _speedMax * yAxisMovement; // Velocidad en el eje Y
+        // Establecemos las velocidades actuales en los ejes X, Y y Z, teniendo en cuenta los limites del campo. 
+        // Si se alcanzo un limite en un eje, la velocidad se establece en cero para ese eje. 
+        // De lo contrario, la velocidad se calcula basandonos en la entrada de joystick correspondiente para ese eje multiplicada por la velocidad maxima correspondiente. 
+        _currentSpeed_X = xBoundaryReached ? 0 : _speedMax * _horizontalInput; // Velocidad en el eje X
+        _currentSpeed_Z = zBoundaryReached ? 0 : _speedMax * _verticalInput; // Velocidad en el eje Z
+        _currentSpeed_Y = yBoundaryReached ? 0 : _speedMax * yAxisMovement; // Velocidad en el eje Y
+
         Vector3 movement = new Vector3(x: _currentSpeed_X, y: _currentSpeed_Y, z: _currentSpeed_Z);
 
         transform.Translate(movement * Time.deltaTime); //Para mover la Skycam
 
         // Delimitador de posicion de posicion (eje x - eje z - eje y)
         Vector3 tempPos = transform.position;
-        CheckBoundaries(ref tempPos.x, _xNegativeBoundary, _xPositiveBoundary);
-        CheckBoundaries(ref tempPos.z, _zNegativeBoundary, _zPositiveBoundary);
-        CheckBoundaries(ref tempPos.y, _heightLimitMin, _heightLimitMax);
+        CheckBoundaries(ref tempPos.x, _xNegativeBoundary, _xPositiveBoundary, ref xBoundaryReached, ref _horizontalInput);
+        CheckBoundaries(ref tempPos.z, _zNegativeBoundary, _zPositiveBoundary, ref zBoundaryReached, ref _verticalInput);
+        CheckBoundaries(ref tempPos.y, _heightLimitMin, _heightLimitMax, ref yBoundaryReached, ref yAxisMovement);
         transform.position = tempPos;
 
         currentHeight = transform.position.y.ToString("N2");
         currentSpeed = movement.magnitude.ToString("N2");
     }
 
-    void CheckBoundaries(ref float position, float negativeBoundary, float positiveBoundary)
+    void CheckBoundaries(ref float position, float negativeBoundary, float positiveBoundary, ref bool boundaryReached, ref float joystickInput)
     {
         position = Mathf.Clamp(position, negativeBoundary, positiveBoundary);
-        Debug.Log("Position" + position.ToString("N2"));
-    }
-
-    void CheckBoundaries1(ref float position, float minBoundary, float maxBoundary)
-    {
-        if ((position >= maxBoundary) || (position <= minBoundary)) {
-            currentSpeed = "0.00";
-        }
+        // Chequeamos si hemos alcanzado el limite de movimiento (ya sea positivo o negativo) en la direccion correspondiente, 
+        // y tambien si todavía estamos moviéndonos en esa dirección (es decir, el valor de entrada del joystick es negativo para el limite negativo o positivo para el limite positivo) 
+        // ya que si no hacemos este chequeo adicional, boundaryReached se establecera en verdadero y la velocidad del eje correspondiente se establece en cero, 
+        // lo que significa que no podemos movernos más en ninguna direccion para el eje correspondiente, y por lo tanto nunca mas cambia el valor de boundaryReached.
+        if (position == negativeBoundary && joystickInput < 0 || position == positiveBoundary && joystickInput > 0)
+        {
+            boundaryReached = true;
+        } 
+        else { boundaryReached = false; }
     }
 }
